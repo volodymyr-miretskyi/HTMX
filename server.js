d098337f9e2fa287d1e289f3c21763984dcab032
Volodymyr Miretskyi <vova.miretskyi@gmail.com>
2025-04-14 20:28:22 +0300

    init

---
+++ server.js
@@ -0,0 +1,11 @@
+import express from "express";
+
+const app = express();
+
+app.use(express.static("public"));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.listen(3000, () => {
+  console.log("Server is running on port 3000");
+});
