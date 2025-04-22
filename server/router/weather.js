9310249bd033c363b2cc75dee5723bb5e91a251d
volodymyr-miretskyi <vova.miretskyi@gmail.com>
2025-04-22 21:48:46 +0300

    weather page: form: added more options

--- server/router/weather.js
+++ server/router/weather.js
@@ -1,4 +1,5 @@
 import { Router } from "express";
+import { format } from "date-fns";
 import axios from "axios";
 
 const router = Router();
@@ -18,21 +19,64 @@ const getLatitudeLongitude = async (city) => {
   };
 };
 
+const mapForecastData = (data) => {
+  const { time, temperature_2m_max, temperature_2m_min } = data;
+
+  const forecast = time.map((date, index) => ({
+    date,
+    max: temperature_2m_max[index],
+    min: temperature_2m_min[index],
+  }));
+
+  return forecast;
+};
+
+const renderWeatherForecastItem = (data) => {
+  const day = format(new Date(data.date), "E");
+  const max = data.max;
+  const min = data.min;
+  const unit = data.unit[0].toUpperCase();
+
+  return `
+    <div class="min-w-28 px-4 py-3 border border-blue-700 bg-blue-950 text-blue-100 rounded-md">
+      <div class='text-2xl font-semibold'>
+        ${day}
+      </div>
+      <div class='text-1xl font-semibold'>
+        ${max} °${unit}
+      </div>
+      <div class='text-1xl font-semibold text-blue-400 opacity-50'>
+        ${min} °${unit}
+      </div>
+    </div>
+  `;
+};
+
 //TODO: finish endpoint
 router.get("/", async (req, res) => {
+  const city = req.query.city || "London";
+  const unit = req.query.unit || "celsius";
+  const days = parseInt(req.query.days) || 7;
+
   try {
-    const { latitude, longitude } = await getLatitudeLongitude("Pidkamin");
+    const { latitude, longitude } = await getLatitudeLongitude(city);
     const params = new URLSearchParams({
       latitude,
       longitude,
+      temperature_unit: unit,
+      forecast_days: days,
+      current: "temperature_2m,is_day,rain",
       daily: "temperature_2m_max,temperature_2m_min",
     });
 
     const result = await axios.get(`${process.env.WEATHER_API_URL}?${params}`);
     console.log(result.data);
+    const forecast = mapForecastData(result.data.daily);
 
-    res.json(
-      `<button type="button" hx-get="/api/weather/poll" hx-trigger="click">Click</button>`
+    res.send(
+      `<div class='grid gap-4 grid-cols-7'>${forecast
+        .map((item) => renderWeatherForecastItem({ ...item, unit }))
+        .join("")}</div>`
     );
   } catch (e) {
     throw new Error(e);
